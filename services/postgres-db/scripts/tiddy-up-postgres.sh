4abe8b9dfc720dca8176e8c95a4cbe02867760cf
thong <ikumen@gnoht.com>
2020-05-28 09:11:17 +0000

    Added postgres service. Remove support for < TLSv1.2. Add general installer/uninstaller for all services

---
+++ services/postgres-db/scripts/tiddy-up-postgres.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  REVOKE CONNECT ON DATABASE $POSTGRES_DB FROM public;
+  REVOKE TEMPORARY ON DATABASE $POSTGRES_DB FROM public;
+  REVOKE ALL ON SCHEMA public FROM public;
+EOSQL
