a6d11f6705715c1cd6e12a3c78c0b0fb8635ebc4
ikumen <ikumen@gnoht.com>
2020-06-10 12:16:14 -0700

    Making install script a little less brittle, extract container and image names iexplicitly instead of relying on service name

--- services/zookeeper/install.sh
+++ services/zookeeper/install.sh
@@ -11,11 +11,19 @@ WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Get ready for installation.
 cd $WORKDIR
 
+# TODO: source in env
+ZK_DATA_DIR=${ZK_DATA_DIR:-/data/zookeeper}
+mkdir -p $ZK_DATA_DIR
+chown -R '1001:root' $ZK_DATA_DIR
+
 # Pull the service name out of our docker compose file, and use it as the service name 
 # for systemd. 
-SERVICE="$(get_service_name $WORKDIR/docker-compose.yml)"
-
-
+SERVICE="$(get_dc_service_name $WORKDIR/docker-compose.yml)"
+if [ -z $SERVICE ]; then
+  echo "Could not determine service name"
+  exit 1
+fi
+  
 # Build the container image
 /usr/bin/docker-compose -f docker-compose.yml up --no-start
 
