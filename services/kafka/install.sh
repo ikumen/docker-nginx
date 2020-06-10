e194e56d06bd5b395c9863e945ddafa02ca75acd
ikumen <ikumen@gnoht.com>
2020-06-10 12:16:22 -0700

    Making install script a little less brittle, extract container and image names iexplicitly instead of relying on service name

--- services/kafka/install.sh
+++ services/kafka/install.sh
@@ -13,8 +13,11 @@ cd $WORKDIR
 
 # Pull the service name out of our docker compose file, and use it as the service name 
 # for systemd. 
-SERVICE="$(get_service_name $WORKDIR/docker-compose.yml)"
-
+SERVICE="$(get_dc_service_name $WORKDIR/docker-compose.yml)"
+if [ -z $SERVICE ]; then
+  echo "Could not determine service name"
+  exit 1
+fi
 
 # Build the container image
 /usr/bin/docker-compose -f docker-compose.yml up --no-start
